99854bccb30257914cc25a4991327eae9ca7110b
annabellachan <annabellac24@gmail.com>
2022-11-14 11:39:21 -0800

    updated again

--- src/App.js
+++ src/App.js
@@ -15,15 +15,25 @@ const App = () => {
     "?",
     "?"
   ])
+  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length))
+  const [bombLocation, setBombLocation] = useState(Math.floor(Math.random() * board.length))
 
   const handleGamePlay = (index) => {
     let updatedBoard = [...board]
     // ... spread operator creates a copy of the state variable board
-    updatedBoard[index] = "🎄"
-    // console.log(updatedBoard)
-    setBoard(updatedBoard)
-    // invokes the function instead of using console.log
-
+    // Makes a copy of the board in state so that we can modify a single instance inside the array
+    if(index === treasureLocation) {
+      // updating a single instance of the copied array to treasure emoji
+      updatedBoard[index] = "🍖"
+      // setting entire updated board to state
+      setBoard(updatedBoard)
+    } else if(index === bombLocation) {
+      updatedBoard[index] = "👿"
+      setBoard(updatedBoard)
+    } else {
+      updatedBoard[index] = "🐶"
+      setBoard(updatedBoard)
+    }
   }
 
   return (
